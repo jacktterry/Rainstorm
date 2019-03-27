bc4f3673216c864a213c611816202c42144d871e
Jack Terry <jacktterry@gmail.com>
2019-03-26 19:22:55 -0500

    Project setup

---
+++ Rainstorm/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RootViewController.swift
+//  Rainstorm
+//
+//  Created by Jack Terry on 3/26/19.
+//  Copyright © 2019 Jack Terry. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
